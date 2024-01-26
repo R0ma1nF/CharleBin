3bb68bc817581ac561dfbd9a0c4d771d33a1e983
Romain Formet <romain.formet@gmail.com>
2024-01-26 14:53:15 +0100

    changement langage par defaut

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -50,7 +50,7 @@ class Configuration
             'info'                     => 'More information on the <a href=\'https://privatebin.info/\'>project page</a>.',
             'notice'                   => '',
             'languageselection'        => false,
-            'languagedefault'          => 'fr',
+            'languagedefault'          => 'en',
             'urlshortener'             => '',
             'qrcode'                   => true,
             'icon'                     => 'identicon',
